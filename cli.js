5fdab4df82693263461f1aa69fff0890307a4057
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-11-03 23:24:57 +0700

    feat: rename to cli and install globally

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const fs = require('fs');
 const configstore = require('configstore');
 const inquirer = require('./lib/inquirer/select-base-project');
 
@@ -12,7 +13,13 @@ console.log(
 
 const run = async () => {
   const credentials = await inquirer.askGithubCredentials();
-  console.log(credentials);
+  _mkdirp(credentials['base project'][0]);
+};
+
+const _mkdirp = p => {
+  if (!fs.existsSync(p)) {
+    fs.mkdirSync(p);
+  }
 };
 
 run();
