544dfe6d6f2093490e008687b5ed5d1e33e99801
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-11-05 22:57:15 +0700

    inquirer for teamName and port

--- cli.js
+++ cli.js
@@ -5,11 +5,15 @@ const fs = require('fs');
 const configstore = require('configstore');
 const baseProjectInquirer = require('./lib/inquirer/base-project');
 const componentNameInquirer = require('./lib/inquirer/component-name');
+const teamNameInquirer = require('./lib/inquirer/team-name');
+const portInquirer = require('./lib/inquirer/port');
 
 clear();
 
 let baseProject;
 let componentName;
+let teamName;
+let port;
 
 console.log(
   chalk.yellow(
@@ -23,11 +27,17 @@ console.log(
 
 const run = async () => {
   let answer = await baseProjectInquirer.ask();
-  // _mkdirp(credentials['base project'][0]);
+  _mkdirp(credentials['base project'][0]);
   baseProject = answer[baseProjectInquirer.questionName][0];
 
   answer = await componentNameInquirer.ask();
   componentName = answer[componentNameInquirer.questionName][0];
+
+  answer = await teamNameInquirer.ask();
+  teamName = answer[teamNameInquirer.questionName][0];
+
+  answer = await portInquirer.ask();
+  port = answer[portInquirer.questionName][0];
 };
 
 const _mkdirp = p => {
