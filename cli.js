fcb8c4b56cb6ea6ddf3340b867bcdb8d60ed4864
Hoang Linh <truonghoanglinhk55bkhn@gmail.com>
2019-11-05 23:14:52 +0700

    feat: create empty folder and copy react template

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const fs = require('fs');
+const fs = require('fs-extra');
+const path = require('path');
 const configstore = require('configstore');
 const baseProjectInquirer = require('./lib/inquirer/base-project');
 const componentNameInquirer = require('./lib/inquirer/component-name');
@@ -14,6 +15,8 @@ let baseProject;
 let componentName;
 let teamName;
 let port;
+const printCyan = text => console.log(` ${chalk.cyan(text)}`);
+const printGreen = text => console.log(` ${chalk.green(text)}`);
 
 console.log(
   chalk.yellow(
@@ -27,23 +30,41 @@ console.log(
 
 const run = async () => {
   let answer = await baseProjectInquirer.ask();
-  _mkdirp(credentials['base project'][0]);
+  // _mkdirp(credentials['base project'][0]);
   baseProject = answer[baseProjectInquirer.questionName][0];
 
   answer = await componentNameInquirer.ask();
-  componentName = answer[componentNameInquirer.questionName][0];
+  componentName = answer[componentNameInquirer.questionName];
 
-  answer = await teamNameInquirer.ask();
-  teamName = answer[teamNameInquirer.questionName][0];
+  // answer = await teamNameInquirer.ask();
+  // teamName = answer[teamNameInquirer.questionName];
 
-  answer = await portInquirer.ask();
-  port = answer[portInquirer.questionName][0];
+  // answer = await portInquirer.ask();
+  // port = answer[portInquirer.questionName];
+
+  createFolder(baseProject, componentName);
 };
 
-const _mkdirp = p => {
-  if (!fs.existsSync(p)) {
-    fs.mkdirSync(p);
-  }
+const createFolder = (baseProject, componentName) => {
+  printCyan(
+    `⏳  Creating ${baseProject} Web Component by the name of ${componentName} ...`
+  );
+  console.log();
+
+  const folderName = baseProject.toLowerCase() + '-' + componentName;
+  fs.ensureDirSync(folderName);
+  fs.emptyDirSync(folderName);
+
+  printCyan('✅  Created project folder.');
+  console.log();
+
+  fs.copySync(
+    path.resolve(__dirname, 'templates/' + baseProject.toLowerCase()),
+    folderName
+  );
+
+  printCyan('✅  Initialized project.');
+  console.log();
 };
 
 run();
